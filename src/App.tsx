5f5449d77fc3190b7431c337cf3e81db49edd955
Alicia Foureaux <aliciafoureauxlucas@gmail.com>
2021-03-15 12:08:58 -0300

    feat: dashboard, summary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyles />
     </>
   );
